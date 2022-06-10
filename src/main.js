f73be38be4571f74e9d70809ba19a27c6ecaf82d
Rohald van Merode <rohald_89@live.nl>
2022-06-10 14:16:49 +0200

    added functionality for mobile nav

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-import './index.css'
+import "./index.css";
+
+const btn = document.querySelector("#menu-btn");
+const menu = document.querySelector("#menu");
+
+const navToggle = () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("hidden");
+  menu.classList.toggle("flex");
+};
+
+btn.addEventListener("click", navToggle);
